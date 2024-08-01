a9ab9b07ae7e348e578c8c1e2a2488f89d459312
volry <v.v.ryzhuk@gmail.com>
2024-08-01 20:17:04 +0300

    normalized sql script

--- task.sql
+++ task.sql
@@ -9,25 +9,42 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
+Create TABLE Warehouses (
+    ID INT AUTO_INCREMENT,
+    Name varchar(50),
+    Address Varchar(250),
+    CountryID INT,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION
+);
+
+
+Create table Products (
+    ID INT AUTO_INCREMENT,
+    Name varchar(50),
+    PRIMARY KEY (ID)
+);
+
+
 CREATE TABLE ProductInventory (
     ID INT,
-    ProductName VARCHAR(50),
+    ProductID INT,
     WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
+    WarehouseID INT,
+    PRIMARY KEY(ID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION
 );
 
 -- Populate test data
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
+INSERT INTO Countries (ID,Name) VALUES (1, 'Country1');
+INSERT INTO Countries (ID,Name) VALUES (2, 'Country2');
+
+INSERT INTO Warehouses (Name, Address, CountryID) VALUES ('Warehouse-1','City-1', 'Street-1',1);
+INSERT INTO Warehouses (Name, Address, CountryID) VALUES ('Warehouse-2','City-2', 'Street-2',2);
+
+INSERT INto Products (Name) VALUES ('Super-ukrainianProduct');
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO ProductInventory (ProductID,WarehouseAmount,WarehouseID) VALUES (1, 2, 1);
+INSERT INTO ProductInventory (ProductID,WarehouseAmount,WarehouseID) VALUES (1, 5, 2);
